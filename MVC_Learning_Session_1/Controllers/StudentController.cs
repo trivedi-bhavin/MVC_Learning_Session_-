e4183499d5f77838dfec5b81aa0002b3a1a39875
Bhavin Trivedi <dearbhavin@hotmail.com>
2019-07-11 10:45:20 +0530

    Submitted Student Object with List of Subjects. Nested C# Objects from JavaScript AJAX call.

---
+++ MVC_Learning_Session_1/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using MVC_Learning_Session_1.Models;
+namespace MVC_Learning_Session_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student obj = Student.GetStudent();
+            return View(obj);
+        }
+
+        //Reference Article - Learning -https://www.pluralsight.com/guides/asp.net-mvc-getting-default-data-binding-right-for-hierarchical-views
+        [HttpPost]
+        public ActionResult SubmitData(Student obj)
+        {
+            Student item = obj;
+           
+            string id = Request["Id"];
+            return Content("Success");
+        }
+        [HttpPost]
+        public ActionResult SubmitJSONData(string json)
+        {
+            Student obj = new JavaScriptSerializer().Deserialize<Student>(json);
+
+            //string id = obj.Id.ToString();
+            string id = Request["Id"];
+            return Content("Success");
+        }
+
+        public JsonResult GetStudentJSON()
+        {
+            Student obj = Student.GetStudent();
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
---
+++ MVC_Learning_Session_1/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Learning_Session_1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public static Student GetStudent()
+        {
+            Student obj = new Student {
+                 Id=101,Name="Tapan" , Subjects= new List<Subject>{
+                 new Subject{ IsSelected=true, SubjectId=1,SubjectName="Math", Status=true},
+                 new Subject{ IsSelected=true,SubjectId=2,SubjectName="Science", Status=true},
+                new Subject{ IsSelected=false, SubjectId=3,SubjectName="English", Status=false}
+            }
+        };
+            return obj;
+        }
+    }
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public bool Status { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+   
+
+}
